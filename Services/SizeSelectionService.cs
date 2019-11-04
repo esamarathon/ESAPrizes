659eac21c51062e61da4fc24c2a05aef23a3dbb6
Johan "Zephyyrr" Fogelström <the@excellent.ninja>
2019-11-04 23:41:52 +0100

    Changed size selection to be based on real production values.

--- Services/SizeSelectionService.cs
+++ Services/SizeSelectionService.cs
@@ -3,11 +3,11 @@ using ESAPrizes.Models;
 namespace ESAPrizes.Services {
     public class SizeSelectionService {
         public string GetSize(Prize p) {
-                if (p.Value % 100 == 0) {
+                if (p.MinimumBid % 50 == 0) {
                     return SizeClass.Large;
                 }
 
-                if (p.Value % 50 == 0) {
+                if (p.MinimumBid % 25 == 0) {
                     return SizeClass.Medium;
                 }
 
