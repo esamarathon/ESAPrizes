a2350979ade2c08569276e4824a58ab2452c8790
Johan "Zephyyrr" Fogelström <the@excellent.ninja>
2019-11-28 22:54:53 +0100

    Initial work on a Modal functionality.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,41 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+class Modal {
+    constructor(contents) {
+        this.contents = contents;
+        this.element = document.createElement("div");
+        this.element.classList.add("modal");
+        document.appendChild(element);
+    }
+
+    open() {
+        this.element.classList.add("open");
+    }
+
+    close() {
+        this.element.classList.remove("open");
+    }
+
+    toggle() {
+        this.element.classList.toggle("open");
+    }
+
+    destroy() {
+        document.removeChild(element);
+    }
+}
+
+var modal = null;
+
+function initializeModals() {
+    document.querySelectorAll(".prize img")
+    .forEach(function(element) {
+        element.onclick(openModal)
+    });
+}
+
+function openModal() {
+    
+}
