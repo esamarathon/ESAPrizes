08093ece71bb8758720b47c3a3de64129afde157
Johan "Zephyyrr" Fogelström <the@excellent.ninja>
2021-02-13 00:55:22 +0100

    Added rule to have single prizes be at least Medium sized.

--- Models/HomeModel.cs
+++ Models/HomeModel.cs
@@ -5,7 +5,6 @@ using System.Linq;
 namespace ESAPrizes.Models {
     public class HomeModel
     {
-        public IEnumerable<Prize> GrandPrizes { get; set; }
         public IEnumerable<IGrouping<Tuple<int, string>, Prize>> Prizes { get; set; } 
     }
 }
--- Services/SizeSelectionService.cs
+++ Services/SizeSelectionService.cs
@@ -1,17 +1,30 @@
 using ESAPrizes.Models;
+#nullable enable
 
 namespace ESAPrizes.Services {
     public class SizeSelectionService {
-        public string GetSize(Prize p) {
-                if (p.MinimumBid % 50 == 0) {
-                    return SizeClass.Large;
-                }
+        public string GetSize(Prize p, int? categorySize) {
+            var defaultSize = GetDefaultSize(p);
 
-                if (p.MinimumBid % 25 == 0) {
-                    return SizeClass.Medium;
-                }
+            if (categorySize != null && 
+                categorySize < 2 && 
+                defaultSize == SizeClass.Small) {
+                return SizeClass.Medium;
+            }
 
-                return SizeClass.Small;
+            return defaultSize;
+        }
+
+        public string GetDefaultSize(Prize p) {
+            if (p.MinimumBid % 50 == 0) {
+                return SizeClass.Large;
+            }
+
+            if (p.MinimumBid % 25 == 0) {
+                return SizeClass.Medium;
+            }
+
+            return SizeClass.Small;
         }
     }
 }
